7150c5484e3bcc7ce990273d30ee2bd3fca31ec9
André Duffeck <aduffeck@suse.com>
2018-12-20 14:10:25 +0100

    Revert "Run the unit tests from the git root"
    This reverts commit 62e1c1620bd3670b09b66b4f0238ecbc45364bbd.

--- pipelines/cf-operator/tasks/test.sh
+++ pipelines/cf-operator/tasks/test.sh
@@ -5,5 +5,4 @@ export PATH=$PATH:$PWD/bin
 export GOPATH=$PWD
 export GO111MODULE=on
 
-cd src/code.cloudfoundry.org/cf-operator
-make test-unit
+make -C src/code.cloudfoundry.org/cf-operator test-unit
