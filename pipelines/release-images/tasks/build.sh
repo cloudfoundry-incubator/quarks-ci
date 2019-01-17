8df4421092f0da9debac62733c2f088654248022
André Duffeck <aduffeck@suse.com>
2019-01-17 09:46:25 +0100

    Trigger on new stemcells

--- pipelines/release-images/tasks/build.sh
+++ pipelines/release-images/tasks/build.sh
@@ -14,14 +14,16 @@ pushd s3.fissile-linux
 tar xfv fissile-*.tgz
 popd
 
-docker pull ${STEMCELL}
-
 VERSION=$(cat release/version)
-STEMCELL_VERSION=${STEMCELL#*fissile-stemcell-}
+
+# Prepare stemcell
+STEMCELL_NAME=${STEMCELL_REPOSITORY}:$(cat s3.stemcell-version/*-version)
+docker pull ${STEMCELL_NAME}
+STEMCELL_VERSION=${STEMCELL_NAME#*fissile-stemcell-}
 STEMCELL_VERSION=${STEMCELL_VERSION/:/-}
 FINAL_NAME=${REGISTRY_NAMESPACE}/${RELEASE_NAME}-release:${STEMCELL_VERSION}-${VERSION}
 
-s3.fissile-linux/fissile build release-images --stemcell=${STEMCELL} --name=${RELEASE_NAME} --version=${VERSION} --sha1=$(cat release/sha1) --url=$(cat release/url)
+s3.fissile-linux/fissile build release-images --stemcell=${STEMCELL_NAME} --name=${RELEASE_NAME} --version=${VERSION} --sha1=$(cat release/sha1) --url=$(cat release/url)
 
 BUILT_IMAGE_ID=$(docker images | egrep ^fissile | awk '{print $3}')
 docker tag ${BUILT_IMAGE_ID} ${FINAL_NAME}
