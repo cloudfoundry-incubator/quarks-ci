2894741d432bec56c94a7b10464fd8f2e61ccb22
Mario Manno <mario.manno@suse.com>
2019-03-20 15:00:20 +0100

    Revert ngrok tunnel setup
    This reverts commit 1d0b903f86aa6c4bbee3f77ba8d3882820ba9565.
    This reverts commit 160871b94a84de4445f997e2a85dabecaffab7d8, reversing
    This reverts commit 31e815c19d8d9eca784156f7fcb28f307299981a, reversing
    changes made to da68e2016a2c36f029ef24c17b0a1058dac7f805.

--- pipelines/cf-operator/tasks/test-integration.sh
+++ pipelines/cf-operator/tasks/test-integration.sh
@@ -4,18 +4,11 @@ set -eu
 export PATH=$PATH:$PWD/bin
 export GOPATH=$PWD
 export GO111MODULE=on
-export OPERATOR_WEBHOOK_PORT=443
 export TEST_NAMESPACE="test$(date +%s)"
-export TUNNEL_NAME="tunnel-${OPERATOR_WEBHOOK_PORT}"
-
-echo "Starting ngrok tunnel"
-ngrok authtoken ${ngrok_token}
-ngrok tcp ${OPERATOR_WEBHOOK_PORT} &
 
 ## Make sure to cleanup the tunnel pod and service
 cleanup () {
   echo "Cleaning up"
-  kill $(pgrep ngrok)
   kubectl delete mutatingwebhookconfiguration cf-operator-mutating-hook-${TEST_NAMESPACE}
   kubectl delete ns ${TEST_NAMESPACE}
 }
@@ -28,43 +21,6 @@ eval $(ibmcloud cs cluster-config "$ibmcloud_cluster" --export)
 
 echo "Seting up bluemix access"
 kubectl create namespace "$TEST_NAMESPACE"
-
-NGROK_HOST=""
-while [ -z ${NGROK_HOST} ]; do
-  NGROK_HOST=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*):.*/\1/p')
-  NGROK_PORT=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..[^"]*:([^"]*).*/\1/p')
-  [ -z "${NGROK_HOST}" ] && (echo "Waiting for end point..."; sleep 5)
-done
-export NGROK_IP=$(host ${NGROK_HOST} | awk '/has address/ { print $4 }')
-echo "End point: ${NGROK_HOST}:${NGROK_PORT}"
-
-echo "Creating webhook kube service"
-cat <<EOF | kubectl create -f - --namespace=${TEST_NAMESPACE}
-apiVersion: v1
-kind: Service
-metadata:
-  name: ${TUNNEL_NAME}
-  labels:
-    run: ${TUNNEL_NAME}
-spec:
-  ports:
-  - port: ${OPERATOR_WEBHOOK_PORT}
-    targetPort: ${NGROK_PORT}
-    protocol: TCP
----
-kind: Endpoints
-apiVersion: v1
-metadata:
-  name: ${TUNNEL_NAME}
-subsets:
-  - addresses:
-      - ip: ${NGROK_IP}
-    ports:
-      - port: ${NGROK_PORT}
-EOF
-
-echo "Setting webhook host IP"
-export OPERATOR_WEBHOOK_HOST=$(kubectl get service -n "$TEST_NAMESPACE" webhook -o json | jq -r .spec.clusterIP)
 
 echo "Running integration tests"
 make -C src/code.cloudfoundry.org/cf-operator test-integration
