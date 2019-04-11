7cd239644d5ff0115d960e1c8820c0ef9000354b
Sakala Venkata Krishna Rohit <rohitsakala@gmail.com>
2019-04-11 10:23:30 +0530

    add build number to coverage

--- pipelines/cf-operator/tasks/coverage.sh
+++ pipelines/cf-operator/tasks/coverage.sh
@@ -5,6 +5,15 @@ export PATH=$PATH:$PWD/bin
 export GOPATH=$PWD
 export GO111MODULE=on
 
+version=
+
+if [ -f s3.build-number/version ]; then
+  version=$(cat s3.build-number/version)
+fi
+
+export BUILD_NUMBER=${version}
+
+
 cp s3.code-coverage*/*.coverprofile src/code.cloudfoundry.org/cf-operator
 sed -i 's/mode: atomic// ; 1s/^/mode: atomic/' src/code.cloudfoundry.org/cf-operator/*.coverprofile
 
