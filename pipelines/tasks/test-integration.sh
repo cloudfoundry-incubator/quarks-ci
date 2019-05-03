3f42c260b6acd9fb34dcb113f8a86de92394e043
ENCALADA <encalada@de.ibm.com>
2019-05-03 15:55:53 +0200

    Move var CF_OPERATOR_WEBHOOK_PORT to CF_OPERATOR_WEBHOOK_SERVICE_PORT
    Based on https://github.com/cloudfoundry-incubator/cf-operator/pull/223

--- pipelines/tasks/test-integration.sh
+++ pipelines/tasks/test-integration.sh
@@ -15,8 +15,8 @@ fi
 export GOVER_FILE=gover-${version}-integration.coverprofile
 
 # Random port to support parallelism with different webhook servers
-export CF_OPERATOR_WEBHOOK_PORT=$(( ( RANDOM % 62000 )  + 2000 ))
-export TUNNEL_NAME="tunnelpod-${CF_OPERATOR_WEBHOOK_PORT}"
+export CF_OPERATOR_WEBHOOK_SERVICE_PORT=$(( ( RANDOM % 62000 )  + 2000 ))
+export TUNNEL_NAME="tunnelpod-${CF_OPERATOR_WEBHOOK_SERVICE_PORT}"
 
 ## Make sure to cleanup the tunnel pod and service
 cleanup () {
@@ -43,7 +43,7 @@ EOF
 chmod 0600 identity
 
 # GatewayPorts option needs to be enabled on ssh server
-ssh -fNT -i identity -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R "${ssh_server_ip}:${CF_OPERATOR_WEBHOOK_PORT}:localhost:${CF_OPERATOR_WEBHOOK_PORT}" "${ssh_server_user}@${ssh_server_ip}"
+ssh -fNT -i identity -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R "${ssh_server_ip}:${CF_OPERATOR_WEBHOOK_SERVICE_PORT}:localhost:${CF_OPERATOR_WEBHOOK_SERVICE_PORT}" "${ssh_server_user}@${ssh_server_ip}"
 
 echo "Seting up webhook on k8s"
 cat <<EOF | kubectl create -f - --namespace=${TEST_NAMESPACE}
@@ -56,7 +56,7 @@ subsets:
   - addresses:
       - ip: ${ssh_server_ip}
     ports:
-      - port: ${CF_OPERATOR_WEBHOOK_PORT}
+      - port: ${CF_OPERATOR_WEBHOOK_SERVICE_PORT}
 EOF
 export CF_OPERATOR_WEBHOOK_SERVICE_HOST="$ssh_server_ip"
 
