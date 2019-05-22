57616786687e127a36035da7cb105f8e556cfa6f
Mario Manno <mario.manno@suse.com>
2019-05-22 15:00:32 +0200

    Run e2e tests in parallel

--- pipelines/tasks/test-integration.sh
+++ pipelines/tasks/test-integration.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/env sh
 set -eu
 
+: "${ibmcloud_apikey:?}"
+: "${ibmcloud_server:?}"
+: "${ibmcloud_region:?}"
+: "${ibmcloud_cluster:?}"
+: "${ssh_server_ip:?}"
+: "${ssh_server_user:?}"
+: "${ssh_server_key:?}"
+: "${OPERATOR_TEST_STORAGE_CLASS:?}"
+: "${DOCKER_IMAGE_REPOSITORY:?}"
+
 export PATH=$PATH:$PWD/bin
 export GOPATH=$PWD
 export GO111MODULE=on
 export TEST_NAMESPACE="test$(date +%s)"
 
+## File used for coverage reporting
 version=
-
 if [ -f s3.build-number/version ]; then
   version=$(cat s3.build-number/version)
 fi
-
 export GOVER_FILE=gover-${version}-integration.coverprofile
-
-# Random port to support parallelism with different webhook servers
-export CF_OPERATOR_WEBHOOK_SERVICE_PORT=$(( ( RANDOM % 62000 )  + 2000 ))
-export TUNNEL_NAME="tunnelpod-${CF_OPERATOR_WEBHOOK_SERVICE_PORT}"
 
 ## Make sure to cleanup the tunnel pod and service
 cleanup () {
   echo "Cleaning up"
   set +e
-  kubectl delete mutatingwebhookconfiguration "cf-operator-mutating-hook-${TEST_NAMESPACE}"
-  kubectl delete ns --wait=false --grace-period=60 "${TEST_NAMESPACE}"
+  kubectl get mutatingwebhookconfiguration -oname | \
+    grep "$TEST_NAMESPACE" | \
+    xargs -n 1 kubectl delete
   pidof ssh | xargs kill
 }
 trap cleanup EXIT
@@ -32,44 +38,50 @@ echo "Seting up bluemix access"
 ibmcloud login -a "$ibmcloud_server" --apikey "$ibmcloud_apikey"
 ibmcloud cs  region-set "$ibmcloud_region"
 eval $(ibmcloud cs cluster-config "$ibmcloud_cluster" --export)
-
 echo "Running integration tests in the ${ibmcloud_cluster} cluster."
 
-echo "Creating namespace"
-kubectl create namespace "$TEST_NAMESPACE"
-
+## Set up SSH tunnels to make our webhook server available to k8s
 echo "Seting up SSH tunnel for webhook"
 cat <<EOF > identity
 $ssh_server_key
 EOF
 chmod 0600 identity
 
-# GatewayPorts option needs to be enabled on ssh server
-ssh -fNT -i identity -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R "${ssh_server_ip}:${CF_OPERATOR_WEBHOOK_SERVICE_PORT}:localhost:${CF_OPERATOR_WEBHOOK_SERVICE_PORT}" "${ssh_server_user}@${ssh_server_ip}"
+# Random base port to support parallelism with different webhook servers
+export CF_OPERATOR_WEBHOOK_SERVICE_PORT=$(( ( RANDOM % 61000 )  + 2000 ))
+export CF_OPERATOR_WEBHOOK_SERVICE_HOST="$ssh_server_ip"
+export NODES=${NODES:-5}
 
-echo "Seting up webhook on k8s"
-cat <<EOF | kubectl create -f - --namespace=${TEST_NAMESPACE}
+echo "Seting up webhooks and namespaces on k8s"
+for i in $(seq 1 "$NODES"); do
+  port=$(( CF_OPERATOR_WEBHOOK_SERVICE_PORT + i ))
+  namespace="${TEST_NAMESPACE}-${i}"
+  tunnel_name="tunnel-$port"
+  kubectl create namespace "$namespace"
+
+  # GatewayPorts option needs to be enabled on ssh server
+  ssh -fNT -i identity -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R "$ssh_server_ip:$port:localhost:$port" "$ssh_server_user@$ssh_server_ip"
+
+  cat <<EOF | kubectl create -f- --namespace="$namespace"
 ---
 kind: Endpoints
 apiVersion: v1
 metadata:
-  name: ${TUNNEL_NAME}
+  name: $tunnel_name
 subsets:
   - addresses:
-      - ip: ${ssh_server_ip}
+      - ip: $ssh_server_ip
     ports:
-      - port: ${CF_OPERATOR_WEBHOOK_SERVICE_PORT}
+      - port: $port
 EOF
-export CF_OPERATOR_WEBHOOK_SERVICE_HOST="$ssh_server_ip"
-
+done
 
 echo "Running integration tests"
 make -C src/code.cloudfoundry.org/cf-operator test-integration
+find src/code.cloudfoundry.org/cf-operator/code-coverage -name gover-*.coverprofile | xargs -r cp -t code-coverage/
 
 echo "Running e2e CLI tests"
-# fix SSL path
+# fix relative SSL path in KUBECONFIG
 kube_path=$(dirname "$KUBECONFIG")
-sed -i 's@certificate-authority: \(.*\)$@certificate-authority: '$kube_path'/\1@' $KUBECONFIG
+sed -i 's@certificate-authority: \(.*\)$@certificate-authority: '$kube_path'/\1@' "$KUBECONFIG"
 make -C src/code.cloudfoundry.org/cf-operator test-cli-e2e
-
-find src/code.cloudfoundry.org/cf-operator/code-coverage -name gover-*.coverprofile | xargs -r cp -t code-coverage/
