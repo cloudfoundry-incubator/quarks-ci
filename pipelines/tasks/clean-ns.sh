06939ef36d18ae23901e5d4b08261a4ebec4b168
ENCALADA <encalada@de.ibm.com>
2019-06-13 13:43:44 +0200

    Add new task clean_ns.sh
    This will looks for namespaces older than one day,
    patch the ejob/ests/bdpl resources and then delete
    the namespace.

---
+++ pipelines/tasks/clean-ns.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+echo "Seting up bluemix access"
+ibmcloud login -a "$ibmcloud_server" --apikey "$ibmcloud_apikey"
+ibmcloud cs region-set "$ibmcloud_region"
+
+echo "Running in cluster: ${ibmcloud_cluster}"
+eval $(ibmcloud cs cluster-config "$ibmcloud_cluster" --export)
+
+CURRENT_DATE=$(date '+%Y-%m-%d')
+export CURRENT_DATE
+NS_LIST=$(kubectl get ns --no-headers -o json | jq -r '.items[] | select(.metadata.creationTimestamp | tostring | contains(env.CURRENT_DATE) | not) | select(.metadata.name | contains("test")) | .metadata.name')
+export NS_LIST
+
+if [ -z "${NS_LIST}" ]; then
+  echo "Currently no namespaces, older than 1 day. Nothing to delete"
+else
+  for DELETE_NS in ${NS_LIST}; do 
+      echo "Going to patch ests, bdpl and ejob resources in the namespace: ${DELETE_NS}"
+      eval "$(kubectl -n "${DELETE_NS}" get ests --no-headers | awk '{print "kubectl patch ests -n ${DELETE_NS} " $1 " --patch '\''{\"metadata\": { \"finalizers\": null }}'\'' --type merge"}')"
+      eval "$(kubectl -n "${DELETE_NS}" get bdpl --no-headers | awk '{print "kubectl patch bdpl -n ${DELETE_NS} " $1 " --patch '\''{\"metadata\": { \"finalizers\": null }}'\'' --type merge"}')"
+      eval "$(kubectl -n "${DELETE_NS}" get ejob --no-headers | awk '{print "kubectl patch ejob -n ${DELETE_NS} " $1 " --patch '\''{\"metadata\": { \"finalizers\": null }}'\'' --type merge"}')"
+
+      echo "Going to delete namespace: ${DELETE_NS}"
+      kubectl delete ns "${DELETE_NS}"
+      echo ""
+  done
+fi
+
