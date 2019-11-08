3d26458a0573c302c61906955c45641e8fb8d5e4
Mario Manno <mario.manno@suse.com>
2019-11-08 17:52:19 +0100

    Be backwards-compatible with VERSION_TAG

--- pipelines/tasks/build.sh
+++ pipelines/tasks/build.sh
@@ -14,6 +14,9 @@ git describe --tags --long || git tag v0.0.0 # Make sure there's always a tag th
 . bin/include/versioning
 popd
 
+# for backwards-compatibility
+ARTIFACT_VERSION=${ARTIFACT_VERSION:-$VERSION_TAG}
+
 make -C src/code.cloudfoundry.org/cf-operator build
 cp src/code.cloudfoundry.org/cf-operator/binaries/cf-operator binaries/cf-operator-$ARTIFACT_VERSION
 echo $ARTIFACT_VERSION > docker/tag
