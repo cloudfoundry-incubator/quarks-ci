d1f35626d4f98c732f97c7e767c1d3961d8f7c2b
Svk Rohit <rohitsakala@gmail.com>
2020-01-23 12:39:27 +0530

    Fix bash script error

--- pipelines/tasks/clean-concourse-workers.sh
+++ pipelines/tasks/clean-concourse-workers.sh
@@ -8,8 +8,8 @@ source .envrc
 day_number=$(date +%u)
 day_number=$((day_number - 1))
 
-quarks_worker_number=(($day_number % 2))
-worker_number=(($day_number % 5))
+quarks_worker_number=$((day_number % 2))
+worker_number=$((day_number % 5))
 
 bosh -n -d concourse recreate worker-quarks/$quarks_worker_number
 bosh -n -d concourse recreate worker/$worker_number
