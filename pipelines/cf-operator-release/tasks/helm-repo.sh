86ec5b5ff01688b642b788b0d774f2c2be303aa8
Mario Manno <mario.manno@suse.com>
2020-05-08 11:01:17 +0200

    [#172644852](https://www.pivotaltracker.com/story/show/172644852)

--- pipelines/cf-operator-release/tasks/helm-repo.sh
+++ pipelines/cf-operator-release/tasks/helm-repo.sh
@@ -5,8 +5,8 @@ set -ev
 url=$( cat s3.release.helm-charts/url )
 version=$( cat s3.release.helm-charts/version )
 
-cp -rv helm-repo/. updated/
-cp -v s3.release.helm-charts/*tgz updated/
+cp -av helm-repo/. updated/
+cp -pv s3.release.helm-charts/*tgz updated/
 
 pushd updated
   helm repo index .
