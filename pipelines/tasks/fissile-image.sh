8d102593c3f249da4504e995da2da3618834749a
Mario Manno <mario.manno@suse.com>
2020-05-29 14:46:49 +0200

    Moved wrong file previously
    * cf-operator-testing-image and quarks-gora share build.yml
    * release-images-cf-deployment has it's own files

--- pipelines/tasks/fissile-image.sh
+++ pipelines/tasks/fissile-image.sh
@@ -1,34 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 exec 3> `basename "$0"`.trace
 BASH_XTRACEFD=3
 
 set -eux
 
-# Start the Docker daemon.
-source docker-image-resource/assets/common.sh
-max_concurrent_downloads=10
-max_concurrent_uploads=10
-insecure_registries=""
-registry_mirror=""
-start_docker \
-  "${max_concurrent_downloads}" \
-  "${max_concurrent_uploads}" \
-  "${insecure_registries}" \
-  "${registry_mirror}"
-trap 'stop_docker' EXIT
+# Start Docker Daemon (and set a trap to stop it once this script is done)
+echo 'DOCKER_OPTS="--data-root /scratch/docker --max-concurrent-downloads 10"' >/etc/default/docker
+service docker start
+service docker status
+trap 'service docker stop' EXIT
+sleep 10
 
-# Login to the Docker registry.
-echo "${DOCKER_TEAM_PASSWORD_RW}" | docker login "${DOCKER_REGISTRY}" --username "${DOCKER_TEAM_USERNAME}" --password-stdin
+echo "${DOCKER_TEAM_PASSWORD_RW}" | docker login --username "${DOCKER_TEAM_USERNAME}" --password-stdin
 
-# Extract the fissile binary.
-tar xvf s3.fissile-linux/fissile-*.tgz --directory "/usr/local/bin/"
+pushd s3.fissile-linux
+tar xfv fissile-*.tgz
+popd
 
-# Pull the stemcell image.
-stemcell_version="$(cat s3.stemcell-version/"${STEMCELL_VERSIONED_FILE##*/}")"
-stemcell_image="${STEMCELL_REPOSITORY}:${stemcell_version}"
-docker pull "${stemcell_image}"
+VERSION=$(cat release/version)
 
-# Build the releases.
-tasks_dir="$(dirname $0)"
-bash <(yq -r ".manifest_version as \$cf_version | .releases[] | select(.name==\"${RELEASE}\") | \"source ${tasks_dir}/fissile-build-release.sh; build_release \\(\$cf_version|@sh) '${DOCKER_REGISTRY}' '${DOCKER_ORGANIZATION}' '${DOCKER_TEAM_USERNAME}' '${DOCKER_TEAM_PASSWORD_RW}' '${STEMCELL_OS}' '${stemcell_version}' '${stemcell_image}' \\(.name|@sh) \\(.url|@sh) \\(.version|@sh) \\(.sha1|@sh)\"" "${CF_DEPLOYMENT_YAML}")
+# Prepare stemcell
+STEMCELL_NAME="${STEMCELL_REPOSITORY}:$(cat s3.stemcell-version/*-version)"
+docker pull "${STEMCELL_NAME}"
+
+sha1=$(sha1sum release/*.tgz | awk '{print $1}')
+s3.fissile-linux/fissile build release-images --stemcell="${STEMCELL_NAME}" --name="${RELEASE_NAME}" --version="${VERSION}" --sha1="$sha1" --url="$(cat release/url)"
+
+BUILT_IMAGE=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -v "$STEMCELL_REPOSITORY" | head -1)
+docker tag "${BUILT_IMAGE}" "${REGISTRY_NAMESPACE}/${BUILT_IMAGE}"
+docker push "${REGISTRY_NAMESPACE}/${BUILT_IMAGE}"
