5881ca7742a51b922353027899e89b5ad31ad639
Mario Manno <mario.manno@suse.com>
2020-05-29 10:22:04 +0200

    Add set-all script to set all pipelines

---
+++ set-all.sh
@@ -0,0 +1,12 @@
+./fly-pipeline cfo cf-operator
+./fly-pipeline cfo cf-operator-check
+./fly-pipeline cfo cf-operator-nightly
+./fly-pipeline cfo cf-operator-testing-image
+./fly-pipeline cfo release-images
+./fly-pipeline cfo release-images-cf-deployment
+./fly-pipeline cfo quarks-gora
+./fly-pipeline cfo images
+
+pushd pipelines/cf-operator-release
+  ./configure.sh cfo v4.0.x v4.
+popd
