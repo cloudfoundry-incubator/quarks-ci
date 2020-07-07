5c778ba57bd69a395b4929959285b8ba446d8f00
Mario Manno <mario.manno@suse.com>
2020-07-07 11:07:10 +0200

    Add SLE version to commit message for bumps

--- pipelines/tasks/bump-patch.sh
+++ pipelines/tasks/bump-patch.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
 
-set -ex
+set -e
 
 echo "==== obs image"
 grep . lftp.obs-docker-stemcell/*
+version=$(find lftp.obs-docker-stemcell -type f | head -1 | sed 's/.*x86_64-//; s/.docker.*//')
+message="Bumping docker image from OBS to '$version'"
 echo
-
 
 echo "==== current git tag"
 cd src
 old=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 echo "Found previous tag: $old"
 ruby -e 'a=ARGV[0].split(/\./); a[-1]=a[-1].to_i+1; print a.join(".")' "$old" > ../tag/name
-echo "Bump version for image update from OBS" > ../tag/message
+echo "$message" > ../tag/message
 echo "Output:"
 grep . ../tag/*
 echo
@@ -23,4 +24,4 @@ cd ../modified
 echo "==== create empty commit to trigger concourse"
 git config --global user.name "CFContainerizationBot"
 git config --global user.email "cf-containerization@cloudfoundry.org"
-git commit -m "Bump base docker image" --allow-empty
+git commit -m "$message" --allow-empty
