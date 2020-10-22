fc7fb06d3f7edbd8706a28db9520cb6bffe7f5da
Mario Manno <mario.manno@suse.com>
2020-10-22 14:36:34 +0200

    Quick fix against 'dirty' in version
    Due to a missing 'go mod tidy', the version string includes '-dirty'

--- pipelines/tasks/build.sh
+++ pipelines/tasks/build.sh
@@ -11,8 +11,10 @@ pushd src/code.cloudfoundry.org/quarks-operator
   bin/tools
   bin/build
   . bin/include/versioning
+  ARTIFACT_VERSION=$( echo "$ARTIFACT_VERSION" | sed 's/-dirty//' )
   echo "$ARTIFACT_VERSION" > "$BASE"/docker/tag
 popd
+
 
 cp src/code.cloudfoundry.org/quarks-operator/binaries/quarks-operator "binaries/quarks-operator-$ARTIFACT_VERSION"
 echo "Built $ARTIFACT_VERSION binary"
