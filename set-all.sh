4206105a9bf22198d88588bb27e9a35a68603627
Mario Manno <mario.manno@suse.com>
2021-02-25 11:50:45 +0100

    Remove old pipelines from set-all.sh

--- set-all.sh
+++ set-all.sh
@@ -4,9 +4,10 @@ target=${1-cfo}
 vars=$(lpass show "Shared-CF-Containerization/ContainerizedCF-CI-Secrets" --notes)
 
 fly:pipeline() {
-  pipeline_name="$1"
+  pipeline_dir="$1"
+  pipeline_name="$2"
 
-  pushd pipelines/"$pipeline_name"
+  pushd pipelines/"$pipeline_dir"
   fly --target "$target" set-pipeline \
     --pipeline="$pipeline_name" \
     --config=<(erb "pipeline.yml") \
@@ -15,17 +16,19 @@ fly:pipeline() {
   popd
 }
 
-fly:pipeline cf-operator
-fly:pipeline cf-operator-check
-fly:pipeline cf-operator-nightly
-fly:pipeline cf-operator-testing-image
-fly:pipeline release-images
-fly:pipeline release-images-cf-deployment
-fly:pipeline quarks-gora
-fly:pipeline images
+fly:pipeline release-images-cf-deployment release-images-cf-deployment
+fly:pipeline quarks-gora quarks-gora
+fly:pipeline images quarks-images
 
-pushd pipelines/cf-operator-release
-  ./configure.sh "$target" v4.0.x v4.
-  ./configure.sh "$target" v5.0.x v5.
-  ./configure.sh "$target" v6.x v6.
-popd
+# old pipelines, now running on Github Actions:
+# fly:pipeline cf-operator
+# fly:pipeline cf-operator-check
+# fly:pipeline cf-operator-nightly
+# fly:pipeline cf-operator-testing-image
+# fly:pipeline release-images
+
+# pushd pipelines/cf-operator-release
+#   ./configure.sh "$target" v4.0.x v4.
+#   ./configure.sh "$target" v5.0.x v5.
+#   ./configure.sh "$target" v6.x v6.
+# popd
